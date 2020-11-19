5bcf090f8e445f1b0ca84492f0a62302e9b32bdc
antonin <antonin.giroire@gmail.com>
2020-11-19 20:18:51 +0100

    Implémentation classe Personnage et Chasseur

---
+++ src/sample/Chasseur.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Chasseur extends Personnage {
+
+    /**
+     * Le chasseur possède ne peut s'équiper que d'arcs
+     */
+    public final static Arc typeArme;
+
+
+    /**
+     * Constructeur d'un Chasseur
+     * @param pointsDeVie   : Nombre de points de vie d'un Chasseur
+     * @param pointsDeMana  : Jauge de points de mana d'un Chasseur
+     * @param niveau        : Niveau d'un Chasseur
+     * @param listeDesArmes : Liste des armes d'un Chasseur
+     */
+    public Chasseur(int pointsDeVie, int pointsDeMana, int niveau, ArrayList<Arme> listeDesArmes) {
+        super(pointsDeVie, pointsDeMana, niveau, listeDesArmes);
+    }
+
+    /**
+     * Le chasseur inflige des dégât à son adversaire avec son arc
+     */
+    public tireArc(Personnage adversaire, Arme arc) {
+        adversaire.setPointsDeVie(adversaire.getPointsDeVie() - arc.get);
+    };
+
+    /**
+     *
+     */
+    public rituelDeLaForet() {
+
+    };
+}
--- src/sample/Personnage.java
+++ src/sample/Personnage.java
@@ -1,4 +1,117 @@
 package sample;
 
-public class Personnage {
+import java.util.ArrayList;
+
+/**
+ * Classe définissant un Personnage de RPG
+ */
+public abstract class Personnage {
+
+    /**
+     * Nombre de points de vie du Personnage
+     */
+    private int pointsDeVie;
+
+    /**
+     * Nombre de points de mana du Personnage
+     */
+    private int pointsDeMana;
+
+    /**
+     * Niveau du Personnage
+     */
+    private int niveau;
+
+    /**
+     * Liste des armes du Personnages
+     */
+    private ArrayList<Arme> listeDesArmes;
+
+    /**
+     * Constructeur d'un Personnage
+     * @param pointsDeVie : Nombre de points de vie d'un Personnage
+     * @param pointsDeMana : Jauge de points de mana d'un Personnage
+     * @param niveau : Niveau d'un Personnage
+     * @param listeDesArmes : Liste des armes d'un Personnage
+     */
+    public Personnage(int pointsDeVie, int pointsDeMana, int niveau, ArrayList<Arme> listeDesArmes) {
+        this.pointsDeVie = pointsDeVie;
+        this.pointsDeMana = pointsDeMana;
+        this.niveau = niveau;
+        this.listeDesArmes = listeDesArmes;
+    }
+
+    /**
+     * Récupère le nombre de points de vie du Personnage
+     * @return pointsDeVie
+     */
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    /**
+     * Modifie le nombre de points de vie du Personnage
+     * @param pointsDeVie : Nombre de points de vie
+     */
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    /**
+     * Récupère le nombre de points de mana du Personnage
+     * @return pointsDeMana
+     */
+    public int getPointsDeMana() {
+        return pointsDeMana;
+    }
+
+    /**
+     * Modifie le nombre de points de mana du Personnage
+     * @param pointsDeMana : Nombre de points de mana
+     */
+    public void setPointsDeMana(int pointsDeMana) {
+        this.pointsDeMana = pointsDeMana;
+    }
+
+    /**
+     * Récupère le niveau du Personnage
+     * @return niveau
+     */
+    public int getNiveau() {
+        return niveau;
+    }
+
+    /**
+     * Modifie le niveau du Personnage
+     * @param niveau
+     */
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    /**
+     * Récupère la liste des armes du Personnage
+     * @return listeDesArmes
+     */
+    public ArrayList<Arme> getListeDesArmes() {
+        return listeDesArmes;
+    }
+
+    /**
+     * Modifie la liste des armes du Personnages
+     * @param listeDesArmes
+     */
+    public void setListeDesArmes(ArrayList<Arme> listeDesArmes) {
+        this.listeDesArmes = listeDesArmes;
+    }
+
+    @Override
+    public String toString() {
+        return "Personnage{" +
+                "pointsDeVie=" + pointsDeVie +
+                ", pointsDeMana=" + pointsDeMana +
+                ", niveau=" + niveau +
+                ", listeDesArmes=" + listeDesArmes +
+                '}';
+    }
 }
